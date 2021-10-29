7458829fc3fc14191ea120922f571dcd359cff63
Younus Farveaz <itsme@theyounus.com>
2021-10-29 17:37:42 +0530

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,8 +75,8 @@
         responsive: [{
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                 }
             },
             {
